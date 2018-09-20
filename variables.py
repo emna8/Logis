f14ebc8c716cbf16b76bb546d30f327b9520417b
emna8 <emna8.github.com>
2018-09-19 23:08:02 -1100

    clickable

--- variables.py
+++ variables.py
@@ -99,6 +99,8 @@ def avg_detail(folder,list_detail_coutries):
     list_annonce=list()
     for c in list_detail_coutries:
         try:
+            path=folder+"\%s.csv"%(c)
+            #rf'{folder}\{c}.csv'
             df=pd.read_csv(rf'{folder}\{c}.csv',encoding="latin 1")
             v=round(df["Moy_prix_by_size"].mean(),2)
             print(v)
